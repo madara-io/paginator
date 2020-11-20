a5f4b7cb79085619b705eba4b86b58d3e692abf2
Edouard CLAUDE <edouard@madara.io>
2020-11-20 10:41:54 +0400

    gorm v2

--- go.mod
+++ go.mod
@@ -1,1 +1,3 @@
 module github.com/madara-io/paginator/v2
+
+go 1.14
--- paginator.go
+++ paginator.go
@@ -1,90 +1,115 @@
 package paginator
 
 import (
+	"errors"
 	"math"
 
 	"gorm.io/gorm"
 )
 
-// Param parameter
-type Param struct {
-	DB      *gorm.DB
-	Page    int
-	Limit   int
-	OrderBy []string
+type Pagination struct {
+	TotalRecords int64 `json:"totalRecords"`
+	TotalPage    int   `json:"totalPage"`
+	Offset       int   `json:"offset"`
+	Limit        int   `json:"limit"`
+	Page         int   `json:"page"`
+	PrevPage     int   `json:"prevPage"`
+	NextPage     int   `json:"nextPage"`
+}
+
+type Paging struct {
+	Page    int      `json:"page"`
+	OrderBy []string `json:"orderBy"`
+	Limit   int      `json:"limit"`
 	ShowSQL bool
 }
 
-// Paginator return
-type Paginator struct {
-	TotalRecord int64       `json:"total_record"`
-	TotalPage   int         `json:"total_page"`
-	Records     interface{} `json:"records"`
-	Offset      int         `json:"offset"`
-	Limit       int         `json:"limit"`
-	Page        int         `json:"page"`
-	PrevPage    int         `json:"prev_page"`
-	NextPage    int         `json:"next_page"`
+type Param struct {
+	DB     *gorm.DB
+	Paging *Paging
 }
 
-// Paging Pagination
-func Paging(p *Param, result interface{}) *Paginator {
-	db := p.DB
+// Endpoint for pagination
+func Pages(p *Param, result interface{}) (paginator *Pagination, err error) {
 
-	if p.ShowSQL {
+	var (
+		done     = make(chan bool, 1)
+		db       = p.DB
+		defPage  = 1
+		defLimit = 20
+		count    int64
+		offset   int
+	)
+
+	// get all counts
+	go getCounts(db, result, done, &count)
+
+	// if not defined
+	if p.Paging == nil {
+		p.Paging = &Paging{}
+	}
+
+	// debug sql
+	if p.Paging.ShowSQL {
 		db = db.Debug()
 	}
-	if p.Page < 1 {
-		p.Page = 1
+	// limit
+	if p.Paging.Limit == 0 {
+		p.Paging.Limit = defLimit
 	}
-	if p.Limit == 0 {
-		p.Limit = 10
+	// page
+	if p.Paging.Page < 1 {
+		p.Paging.Page = defPage
+	} else if p.Paging.Page > 1 {
+		offset = (p.Paging.Page - 1) * p.Paging.Limit
 	}
-	if len(p.OrderBy) > 0 {
-		for _, o := range p.OrderBy {
+	// sort
+	if len(p.Paging.OrderBy) > 0 {
+		for _, o := range p.Paging.OrderBy {
 			db = db.Order(o)
 		}
-	}
-
-	done := make(chan bool, 1)
-	var paginator Paginator
-	var count int64
-	var offset int
-
-	go countRecords(db, result, done, &count)
-
-	if p.Page == 1 {
-		offset = 0
 	} else {
-		offset = (p.Page - 1) * p.Limit
+		str := "id desc"
+		p.Paging.OrderBy = append(p.Paging.OrderBy, str)
 	}
 
-	db.Limit(p.Limit).Offset(offset).Find(result)
+	// get
+	if errGet := db.Limit(p.Paging.Limit).Offset(offset).Find(result).Error; errGet != nil && !errors.Is(errGet, gorm.ErrRecordNotFound) {
+		return nil, errGet
+	}
 	<-done
 
-	paginator.TotalRecord = count
-	paginator.Records = result
-	paginator.Page = p.Page
+	// total pages
+	total := int(math.Ceil(float64(count) / float64(p.Paging.Limit)))
 
-	paginator.Offset = offset
-	paginator.Limit = p.Limit
-	paginator.TotalPage = int(math.Ceil(float64(count) / float64(p.Limit)))
-
-	if p.Page > 1 {
-		paginator.PrevPage = p.Page - 1
-	} else {
-		paginator.PrevPage = p.Page
+	// construct pagination
+	paginator = &Pagination{
+		TotalRecords: count,
+		Page:         p.Paging.Page,
+		Offset:       offset,
+		Limit:        p.Paging.Limit,
+		TotalPage:    total,
+		PrevPage:     p.Paging.Page,
+		NextPage:     p.Paging.Page,
 	}
 
-	if p.Page == paginator.TotalPage {
-		paginator.NextPage = p.Page
-	} else {
-		paginator.NextPage = p.Page + 1
+	// prev page
+	if p.Paging.Page > 1 {
+		paginator.PrevPage = p.Paging.Page - 1
 	}
-	return &paginator
+	// next page
+	if p.Paging.Page != paginator.TotalPage {
+		paginator.NextPage = p.Paging.Page + 1
+	}
+
+	return paginator, nil
 }
 
-func countRecords(db *gorm.DB, anyType interface{}, done chan bool, count *int64) {
+func getCounts(db *gorm.DB, anyType interface{}, done chan bool, count *int64) {
 	db.Model(anyType).Count(count)
 	done <- true
+}
+
+func (p Pagination) IsEmpty() bool {
+	return p.TotalRecords <= 0
 }
